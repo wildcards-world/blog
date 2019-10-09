d15cfd4869f25da9694007adc9aed0473d6fcc15
denhampreen <denhampreen@gmail.com>
2019-10-09 16:03:34 +0200

    Updates

--- content/blog/welcome-to-wildcarsd/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Welcome to Wildcards
-date: "2019-10-08T23:46:37.121Z"
----
-
-Hi Everyone Welcome to wildcards
---
+++ styles-725ac9c025970698a11c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{191:function(n,o,w){},192:function(n,o,w){}}]);
+//# sourceMappingURL=styles-725ac9c025970698a11c.js.map
